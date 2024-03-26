b7c5dcae2377cb43b82111dafecf3668381ec734
Victor Dias Noleto <lzero1541@gmail.com>
2024-03-26 16:43:38 -0300

    metodos para validar

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 import ValidarCpf from './validar-cpf.js';
 
 const cpf = document.querySelector('#cpf');
-const validarCpf = new ValidarCpf(cpf);
-
-console.log(validarCpf);
+const validarCpf = new ValidarCpf(cpf).iniciar();
